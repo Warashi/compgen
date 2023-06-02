03ce40b2e4071a77d1641f3f51b755eac064ed7d
Shinnosuke Sawada <6warashi9@gmail.com>
2023-06-02 19:31:54 +0900

    test: add validation test

---
+++ complexitytest/mul-field-error/complexity_test.go
@@ -0,0 +1,19 @@
+package complexity_test
+
+import (
+	"testing"
+
+	"github.com/99designs/gqlgen/api"
+	"github.com/99designs/gqlgen/codegen/config"
+	"github.com/Warashi/compgen"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFail(t *testing.T) {
+	cfg, err := config.LoadConfigFromDefaultLocations()
+	require.NoError(t, err)
+
+	err = api.Generate(cfg, api.AddPlugin(compgen.New()))
+	assert.ErrorIs(t, err, compgen.ErrMulFieldIsNotInt)
+}
